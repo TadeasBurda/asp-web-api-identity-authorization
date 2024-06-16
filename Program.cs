40631a0aceaaa5f58b694eedd5850884e8ab2e46
Tadeas Burda <tadeas.burda@sensoneo.com>
2024-06-16 12:48:58 +0200

    Add authorization service to DI container
    This commit introduces the addition of the `AddAuthorization()` method call to the `Program.cs` file, effectively registering the Authorization service in the Dependency Injection (DI) container. This change is pivotal for enabling authorization capabilities within the ASP.NET Core application, allowing for the management of access to resources based on user roles or policies. The update signifies a crucial step in enhancing the security of the application by ensuring that certain parts are accessible only to users with the necessary permissions. This modification is part of the broader effort to configure the application's services and middleware, specifically before the HTTP request pipeline configuration and the enabling of Swagger in a development environment.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ var app = builder.Build();
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseInMemoryDatabase("AppDb"));
 
+// Add services to the container.
+builder.Services.AddAuthorization();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
